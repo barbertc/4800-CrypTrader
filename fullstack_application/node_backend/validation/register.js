3a090602c438567787702cc5620cb4e11af603b9
barbertc <barbertc@appstate.edu>
2022-03-17 10:13:26 -0400

    database actually works

--- fullstack_application/node_backend/validation/register.js
+++ fullstack_application/node_backend/validation/register.js
@@ -9,6 +9,10 @@ module.exports = function validateRegisterInput(data) {
     data.password = !isEmpty(data.password) ? data.password: ''
     data.password2 = !isEmpty(data.password2) ? data.password2: ''
 
+    if (Validator.isEmpty(data.name)) {
+        errors.name = 'Name is required'
+    }
+
     if (Validator.isEmpty(data.email)) {
         errors.email = 'Email is required'
     }
