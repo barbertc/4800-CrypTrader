7bc27a092bea0bf868f6b41b99ea5cfb2045335e
barbertc <barbertc@appstate.edu>
2022-03-20 18:38:50 -0400

    temporarily changed front end to a redux powered login

--- fullstack_application/react_client/src/App.js
+++ fullstack_application/react_client/src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -12,12 +21,16 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <footer className="App-footer">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>{!data ? "Loading..." : data}</p>
-      </footer>
-    </div>
+    <Provider store={store}>
+      <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+    </Provider>
   );
 }
 
